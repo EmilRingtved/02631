758d3394693bc1ada64e1556313ca90a9ced4874
EmilRingtved <e.ringtved@gmail.com>
2022-09-24 22:02:45 +0200

    fixed assignments to work in CodeJudge.

--- PythonModule/Week4/BacteriaGrowth.py
+++ PythonModule/Week4/BacteriaGrowth.py
@@ -1,11 +1,11 @@
 from pickle import TRUE
 import numpy as np
 def bacteriaGrowth(n0, alpha, k, N):
+    if N <= n0:
+        return 0
     tN = 1
     while(TRUE):
-        print(n0)
         n = (1 + alpha * (1-(n0/k)))*n0
-        print(n)
         if n < N:
             n0 = n
             tN = tN + 1
--- PythonModule/Week4/FermentationRate.py
+++ PythonModule/Week4/FermentationRate.py
@@ -1,12 +1,10 @@
 import numpy as np
-
-def fermantationRate(measuredRate, lowerBound,upperBound):
+def fermentationRate(measuredRate, lowerBound,upperBound):
     arraySum = 0
     arrayLen = len(measuredRate)
     for i in range(len(measuredRate)): #loop through the array of meassurements
-        if (lowerBound <= measuredRate[i]) and (measuredRate[i] <= upperBound): #check bounds
+        if (lowerBound < measuredRate[i]) and (measuredRate[i] < upperBound): #check bounds
             arraySum += measuredRate[i] #if within bounds add to the sum
         else:
             arrayLen = arrayLen - 1
     return (arraySum / arrayLen) #return calculated average
-    
