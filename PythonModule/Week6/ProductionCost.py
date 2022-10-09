b2cac7dc82240c1f9484a2d03b4d49f9120ca96a
EmilRingtved <e.ringtved@gmail.com>
2022-10-10 01:10:58 +0200

    Close to solving

--- PythonModule/Week6/ProductionCost.py
+++ PythonModule/Week6/ProductionCost.py
@@ -1,6 +1,6 @@
 import numpy as np
 def computeItemCost (resourceMatrix, resourceCost):
-    itemCost = np.array(resourceCost) #sets the len of the returned array
-    for i in range(len(resourceMatrix[1])):
+    itemCost = np.array(resourceMatrix[1]*0.0) #sets the len of the returned array
+    for i in range(0,len(resourceMatrix[1])):
        itemCost[i] = np.dot(resourceMatrix[:,i],resourceCost)#select each coloum of the matrix and dot it with the resource cost vector
     return itemCost
