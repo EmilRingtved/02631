8a2c421b610c9d8d4784efb14e8fccb852b002b3
EmilRingtved <e.ringtved@gmail.com>
2022-10-11 22:09:40 +0200

    Week6 complete

--- PythonModule/Week6/LanguageDetection.py
+++ PythonModule/Week6/LanguageDetection.py
@@ -1,7 +1,12 @@
 import numpy as np
 
-def computeLanguageError():
-    f  = np.loadtxt(open('02631\PythonModule\Week6\letter_frequencies.csv',"rb"),delimiter=",",skiprows=1,usecols = (1,2,3,4,5,6,7,8,9,10))
-    res = np.array(list(f)).astype("float")
-    return(res)
-print(computeLanguageError())
+def computeLanguageError(Ft):
+    file  = np.loadtxt(open('letter_frequencies.csv',"rb"),delimiter=",",skiprows=1,usecols = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15))
+    Fl = np.array(list(file)).astype("float")
+    E = np.zeros(15)
+    for i in range(15):
+        SE = 0
+        for j in range(26):
+            SE = SE + np.sum(np.square(np.subtract(Ft[j], Fl[j,i])))
+        E[i] = SE
+    return(E)
